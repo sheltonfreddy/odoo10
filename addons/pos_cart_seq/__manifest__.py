218ee015d954cbb40b885311e6820dbeaa1666a8
shelton <shelton@zbeanztech.com>
2016-11-10 16:43:40 +0530

    v1.0

---
+++ addons/pos_cart_seq/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': "POS Cart Sequence",
+    'summary': """
+        Add sequence number in cart and receipt""",
+    'description': """
+        Add sequence number in cart and receipt
+    """,
+    'category': 'POS',
+    'version': '1.0',
+    'author' : 'Shelton',
+    'depends': ['point_of_sale'],
+    'data': [
+        #'security/ir.model.access.csv',
+        #'views/template.xml',
+        #'views/product_template_view.xml',
+        #'views/website_sale_templates.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
